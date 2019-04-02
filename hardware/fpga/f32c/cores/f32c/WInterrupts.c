62e73a3e677825bb34d6f9492424f16c76a58144
lawrie <lawrie.griffiths@ntlworld.com>
2019-04-02 20:09:41 +0100

    Support for multiple pin interrupts

--- hardware/fpga/f32c/cores/f32c/WInterrupts.c
+++ hardware/fpga/f32c/cores/f32c/WInterrupts.c
@@ -5,13 +5,22 @@
 __BEGIN_DECLS
 
 void attachInterrupt(uint32_t pin, void (*callback)(void), uint32_t mode) {
-  (*(volatile uint32_t*)IO_PIN_INTERRUPT) = callback;
-  (*(volatile uint32_t*)IO_PIN_INTERRUPT_CONFIG) = (mode == FALLING ? 2 : 0) | (mode == RISING ? 1 : 0);
+  (*(volatile uint32_t*)IO_PIN_INTERRUPT_MASK) = 0xFFFFFFFF;
+  (*(volatile uint32_t*)(IO_PIN_INTERRUPT + (pin << 2))) = callback;
+
+  if (mode == RISING || mode == CHANGE) {
+    (*(volatile uint32_t*)IO_PIN_INTERRUPT_RISING) |=  (1 << pin);
+  }
+
+  if (mode == FALLING || mode == CHANGE) {
+    (*(volatile uint32_t*)IO_PIN_INTERRUPT_FALLING) |=  (1 << pin);
+  }
 }
 
 void detachInterrupt(uint32_t pin) {
-  (*(volatile uint32_t*)IO_PIN_INTERRUPT) = 0;
-  (*(volatile uint32_t*)IO_PIN_INTERRUPT_CONFIG) = 0;
+  (*(volatile uint32_t*)(IO_PIN_INTERRUPT + (pin << 2))) = 0;
+  (*(volatile uint32_t*)IO_PIN_INTERRUPT_RISING) &= ~(1 << pin);
+  (*(volatile uint32_t*)IO_PIN_INTERRUPT_FALLING) &= ~(1 << pin);
 }
  
 __END_DECLS
