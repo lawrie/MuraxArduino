e26b4692ccb2af805ec98e678572697d230c84b3
lawrie <lawrie.griffiths@ntlworld.com>
2019-04-24 14:05:06 +0100

    Mux for 2nd 7-segment display

--- libraries/SevenSegment/SevenSegment.cpp
+++ libraries/SevenSegment/SevenSegment.cpp
@@ -4,10 +4,11 @@
 void
 SevenSegmentClass::write(int data) {
   if (_channel == 0) {
-    // Channel 0 needs pins setting to output
+    // Channel 0 needs pins setting to output and mux 4 setting
     (*(volatile uint32_t*)IO_GPIOB_CTL) |= 0xFF;
+    (*(volatile uint32_t*)IO_MUX) |= 0x10;
   } else if (_channel == 1) { 
-    // Channel 1 needs the pins set to output and the mux setting
+    // Channel 1 needs the pins set to output and mux 2 setting
     (*(volatile uint32_t*)IO_GPIO_CTL) |= 0xF0F0000;
     (*(volatile uint32_t*)IO_MUX) |= 0x04;
   }
