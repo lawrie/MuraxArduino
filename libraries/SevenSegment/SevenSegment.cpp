92a99ccfe18e3ce78a0dabfdb8131e49d90808f3
lawrie <lawrie.griffiths@ntlworld.com>
2019-04-24 09:41:15 +0100

    Support for multiple channels

--- libraries/SevenSegment/SevenSegment.cpp
+++ libraries/SevenSegment/SevenSegment.cpp
@@ -1,8 +1,18 @@
 #include "SevenSegment.h"
+#include <dev/io.h>
 
 void
 SevenSegmentClass::write(int data) {
-  IO_SEVEN_SEGMENT = data;
+  if (_channel == 0) {
+    // Channel 0 needs pins setting to output
+    (*(volatile uint32_t*)IO_GPIOB_CTL) |= 0xFF;
+  } else if (_channel == 1) { 
+    // Channel 1 needs the pins set to output and the mux setting
+    (*(volatile uint32_t*)IO_GPIO_CTL) |= 0xF0F0000;
+    (*(volatile uint32_t*)IO_MUX) |= 0x04;
+  }
+ 
+  IO_SEVEN_SEGMENT[_channel * 0x2000] = data;
 }
 
-SevenSegmentClass SevenSegment;
+SevenSegmentClass SevenSegment(0);
--- libraries/SevenSegment/SevenSegment.h
+++ libraries/SevenSegment/SevenSegment.h
@@ -1,11 +1,14 @@
 #ifndef _SEVEN_SEGMENT_CLASS_
 #define _SEVEN_SEGMENT_CLASS_
 
-#define IO_SEVEN_SEGMENT (*(volatile uint32_t*)0xF0090000)
+#define IO_SEVEN_SEGMENT ((volatile uint32_t*)0xF0090000)
 
 class SevenSegmentClass {
   public:
+    SevenSegmentClass(uint8_t channel) : _channel(channel) {}
     void write(int data);
+  private:
+    uint8_t _channel;
 };
 
 extern SevenSegmentClass SevenSegment;
